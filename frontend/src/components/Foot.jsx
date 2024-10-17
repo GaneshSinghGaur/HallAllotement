431a6d5176b53096d5b2f0c9e0e028078e3f74ee
GANESH SINGH GAUR <112327326+GaneshSinghGaur@users.noreply.github.com>
2024-10-17 23:08:08 +0530

    linkedIn updated

--- frontend/src/components/Foot.jsx
+++ frontend/src/components/Foot.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <p className="text-xl font-semibold">Ganesh Singh @NIT Rourkela</p>
         <div>
-          <a href="http://www.linkedin.com/in/pramod-yadav-418695268" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Linkedin</a>
+          <a href="https://www.linkedin.com/in/ganesh-singh-gaur-21780b23b/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">My Linkedin</a>
           {/* <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Link 2</a>
           <a href="#" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Link 3</a> */}
         </div>
